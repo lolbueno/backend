16beaa9d928d7c7fd13d45fe5f27f4ba1dff15ab
lolbueno <lorenzo_medina93@hotmail.com>
2019-04-10 21:26:30 -0300

    Estrutura back-end

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require ('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+   socket.on('connectRoom', box => {
+       socket.join(box);
+   })   
+    /* console.log("ok");*/
+});
+
+mongoose.connect(
+    'mongodb+srv://omnistack:omnistack@cluster0-ghipq.mongodb.net/OmniStackDB?retryWrites=true', 
+{    
+    useNewUrlParser: true
+}
+);
+
+// middleware local
+app.use((req, res, next) => {
+    req.io = io;
+    return next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
+
+app.use(require("./routes"));
+
+server.listen(3333);
